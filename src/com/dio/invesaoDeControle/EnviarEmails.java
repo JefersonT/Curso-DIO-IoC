a545732eea6873be28539cdf91f52157d1b47c32
JefersonT <jefersontorres.f@gmail.com>
2021-11-20 10:45:31 -0300

    First Commit

---
+++ src/com/dio/invesaoDeControle/EnviarEmails.java
@@ -0,0 +1,16 @@
+package com.dio.invesaoDeControle;
+
+public class EnviarEmails {
+    public EnviarEmails(String tipo, String endereco, String senha) {
+
+    }
+
+    public static EnviarEmails obterDados(){
+        return new EnviarEmails("stmp", "contato@email.com", "senha");
+    }
+
+    public void retornar(String mensagem){
+        System.out.println("Email enviado!");
+    }
+
+}
